db753649b169604e91e9f7fda3692449d82dae51
Qzattitude <meemmeen1990@gmail.com>
2022-02-08 02:23:02 +0600

    Add project files.

---
+++ medGet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace medGet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
