dff866617346e42681cf8105d9af33dd820b0b63
meemmeen1990 gmail.com <meemmeen1990@gmail.com>
2022-07-04 11:15:47 +0600

    CompleteSystem

--- medGet/Controllers/ReportController.cs
+++ medGet/Controllers/ReportController.cs
@@ -2,6 +2,7 @@
 using FastReport.Web;
 using medGet.Controllers.DbController;
 using medGet.Models;
+using medGet.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,9 +34,18 @@ namespace medGet.Controllers
         }
         public IActionResult GetCartByUserNameAndOrderSatus()
         {
+            var context = _db.OrderProduct.Where(p => p.OrderStatus.Equals(false)).AsQueryable();
             var currentuser = _userManager.GetUserName(HttpContext.User);
+            var pricelist = context.Select(q => q.TotalCostProduct).ToList();
+            double amount = 0.0d;
+            PaymentViewModel model = new PaymentViewModel();
+            foreach (var cost in pricelist)
+            {
+                amount += cost;
+            }
+
             WebReport web = new WebReport();
-            var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\OrdderReport.frx";
+            var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\OrderReport.frx";
             web.Report.Load(path);
             var mssqlDataConnection = new MsSqlDataConnection();
             mssqlDataConnection.ConnectionString = _configuration.GetConnectionString("DefaultConnection");
@@ -43,6 +53,7 @@ namespace medGet.Controllers
             web.Report.SetParameterValue("CONN", Conn);
             web.Report.SetParameterValue("UserName", currentuser);
             web.Report.SetParameterValue("OrderStatus", 0);
+            web.Report.SetParameterValue("TotalCost", Math.Round(amount, 2));
             return View(web);
         }
     }
